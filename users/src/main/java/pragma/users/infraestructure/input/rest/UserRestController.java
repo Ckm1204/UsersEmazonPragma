f63a287c9e08415cb93235cee44a4ff0b40c15b4
Camilo Tello <camilotello04@gmail.com>
2024-10-05 11:52:40 -0500

    open api configurado

--- users/src/main/java/pragma/users/infraestructure/input/rest/UserRestController.java
+++ users/src/main/java/pragma/users/infraestructure/input/rest/UserRestController.java
@@ -1,6 +1,11 @@
 package pragma.users.infraestructure.input.rest;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +17,7 @@ import pragma.users.application.service.UserService;
 
 @RestController
 @RequestMapping("/users/")
+@Tag(name = "User Controller", description = "Endpoints for user management")
 public class UserRestController {
 
     private final UserService userService;
@@ -21,27 +27,37 @@ public class UserRestController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Create a new admin user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Admin user created successfully", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+    })
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("admin")
-    public ResponseEntity<Void> saveUserAdmin(@RequestBody UserRequestDTO userRequestDTO) {
+    public ResponseEntity<Void> saveUserAdmin(@RequestBody @Valid UserRequestDTO userRequestDTO) {
         userService.createUserAdmin(userRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-
-
-
+    @Operation(summary = "Create a new auxiliary warehouse user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Auxiliary warehouse user created successfully", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+    })
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("auxBodega")
-    public ResponseEntity<Void> saveUserAuxBodega(@RequestBody  UserRequestDTO userRequestDTO) {
+    public ResponseEntity<Void> saveUserAuxBodega(@RequestBody @Valid UserRequestDTO userRequestDTO) {
         userService.createUserAuxBodega(userRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-
-
+    @Operation(summary = "Create a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+    })
     @PostMapping("user")
-    public ResponseEntity<Void> saveUser(@RequestBody  UserRequestDTO userRequestDTO) {
+    public ResponseEntity<Void> saveUser(@RequestBody @Valid UserRequestDTO userRequestDTO) {
         userService.createUser(userRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
